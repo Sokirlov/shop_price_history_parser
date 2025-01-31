0fda86e2b202e8b0c6e5dc4b884898362672fd6e
sokirlov <soxwhite@gmail.com>
2025-01-31 12:29:22 +0200

    update parser

--- main.py
+++ main.py
@@ -18,18 +18,7 @@ def run_silpo():
 
 
 # Запускаємо парсер сільпо кожного дня о 9:30
-schedule.every().day.at("9:30").do(run_silpo)
+schedule.every().day.at("09:30").do(run_silpo)
 
 if __name__ == '__main__':
     run_silpo()
-    # while True:
-    #     schedule.run_pending()
-    #     time.sleep(60)
-
-
-# if __name__ == '__main__':
-#     silpo = Silpo()
-#     silpo.fetch_index_page()
-#     pages = silpo.build_pages(Silpo)
-#     print(f'I have {len(pages)} pages')
-#     run_scraper(pages)
